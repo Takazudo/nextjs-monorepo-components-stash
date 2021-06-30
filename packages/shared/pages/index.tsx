76d5c2032cb952357894d81c72df80241eacac68
Takeshi Takatsudo <takazudo@gmail.com>
2021-06-30 21:41:15 +0900

    emotion/css

--- packages/shared/pages/index.tsx
+++ packages/shared/pages/index.tsx
@@ -1,8 +1,27 @@
+import { css } from '@emotion/css'
+
+const styles = {
+  richtext: css`
+    h2 {
+      border: 2px solid red;
+    }
+    p {
+      border: 2px solid blue;
+    }
+  `
+}
+
 export default function Home() {
   return (
     <div className="px-12 py-8">
       <h1 className="font-bold text-xl pb-2">Shared package</h1>
       <p>The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. </p>
+      <div className={styles.richtext}>
+        <h2>The quick brown fox jumps over the lazy dog. </h2>
+        <p>The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. </p>
+        <p>The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. </p>
+        <p>The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. </p>
+      </div>
       <div className="w-[80px] h-[80px] inline-block bg-my-orange"></div>
       <div className="w-[80px] h-[80px] inline-block bg-my-orange2"></div>
       <div className="w-[80px] h-[80px] inline-block bg-my-red"></div>

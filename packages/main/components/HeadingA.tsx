6bb775c668f2edfc87f32793a43ae7e69078f190
Takeshi Takatsudo <takazudo@gmail.com>
2021-07-02 21:42:28 +0900

    remove component

--- packages/main/components/HeadingA.tsx
+++
@@ -1,5 +0,0 @@
-export const HeadingA: React.FC = ({ children }) => (
-  <div className="font-bold md:text-lg lg:text-base xl:text-xl">
-    <span className="inline-block border-b border-my-black">{children}</span>
-  </div>
-);
